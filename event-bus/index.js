9888504bbbf1e0503c3bda90fe98734f8a6db27f
Rakshit Saxena <saxena.rakshit@gmail.com>
2021-04-10 21:41:30 +0530

    add query and moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,32 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
+    events.push(event);
+
     //call other services
     //assumption that these calls will pass
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      res.send({ status: 'OK' });
+});
 
-    res.send({status: 'OK' });
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
---
+++ moderation/index.js
@@ -0,0 +1,34 @@
+//check comment created and then check
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+    //if the commenat contains "orange", we reject it
+    //else we emit an event
+    const { type, data } = req.body;
+
+    if(type === 'CommentCreated'){
+        const status = data.content.includes('orange') ? 'rejected': 'approved';  //content is the comment itself
+        console.log('The status is ' + status );
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: { //we will probably need documentaion of each service to know what are the props present in this event.
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+
+    res.send({}); //this needs to be done else the request handler hangs.
+});
+
+app.listen(4003, () =>{
+    console.log('Listening on 4003');
+});
+
