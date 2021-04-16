b16850a8c4d7299fb3c86c03244f3a4a9fc200e5
Rakshit Saxena <saxena.rakshit@gmail.com>
2021-04-16 12:32:02 +0530

    enable ingress-nginx

--- posts/index.js
+++ posts/index.js
@@ -11,6 +11,7 @@ app.use(cors());
 const posts = {};
 
 
+//just for testing
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
@@ -20,7 +21,7 @@ app.post('/events', (req, res) =>{
     res.send({});
 });
 
-app.post('/posts', async (req,res) => {
+app.post('/posts/create', async (req,res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
